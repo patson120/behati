cf05561e37f4ddb1a26f284c6fc376715e920d32
patson120 <patrickkennenl@gmail.com>
2024-02-23 17:25:09 +0100

    Adding a new method  to POST data on users route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -27,8 +27,8 @@ export async function GET(request: Request){
   return NextResponse.json({ message: "List of users", users }, { status: STATUS.OK });
 }
 
-// // Handles POST requests to /api/persons
-// export async function POST(request: Request) {
-//   // ...
-//   return NextResponse.json({ message: "Hello World" }, { status: STATUS.BAD_REQUEST });
-// }
+// Handles POST requests to /api/persons
+export async function POST(request: Request) {
+  // ...
+  return NextResponse.json({ message: "Hello World" }, { status: STATUS.BAD_REQUEST });
+}
