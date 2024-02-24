81d2e266d008be76341cd57b54b1fcb855963230
patson120 <patrickkennenl@gmail.com>
2024-02-24 14:32:37 +0100

    Add Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,6 @@ const Footer = () => {
   const date = new Date()
   return (
     <footer className='black_color'>
-
       <section className='mx-auto max-w-7xl h-44 flex flex-row justify-center items-center'>
         <div className='flex justify-between items-center'>
           <div className='flex flex-col gap-3 w-1/3'>
@@ -23,7 +22,6 @@ const Footer = () => {
               La marque qui vous propose des produits agroalimentaires 100% bio, cultivés sans engrais dans les hautes montagnes de l’ouest Cameroun.
             </p>
           </div>
-
           <div className="flex space-x-4 text-white">
             {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
             <Link href="#" className="nav_item_menu" aria-current="page">ACCUEIL</Link>
@@ -34,7 +32,6 @@ const Footer = () => {
           </div>
         </div>
       </section>
-
       <section className='h-12 bg-white flex flex-1 justify-center items-center'>
         <div className='mx-auto max-w-7xl flex flex-1 flex-row justify-between items-center'>
           <p className='text-md'>
