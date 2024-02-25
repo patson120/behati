9347cba3885a93d07881b132b613abaeafd61663
patson120 <patrickkennenl@gmail.com>
2024-02-25 16:09:25 +0100

    Mise en place du responsive sur le bloc Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <>
       {/* Image section */}
-      <section className="flex flex-1 flex-shrink-0 w-full h-[650px] relative">
+      <section className="flex flex-1 flex-shrink-0 w-full h-[650px] max-lg:h-[500px] max-sm:h-[300px] relative ">
         <div className="h-full w-1/4 flex-shrink-0">
           <img src="/assets/images/rectangle_1.png"
             className="w-full h-full" alt="Produit Camerounais" />
@@ -28,23 +28,27 @@ const Hero = () => {
           className="absolute top-0 right-0 bottom-0 left-0">
 
 
-          <div className=" margin flex flex-col translate-y-1/3 w-2/5 gap-10">/
+          <div className="margin flex flex-col 
+              translate-y-2/3 max-md:translate-y-3/4 max-lg:translate-y-1/3 max-sm:translate-y-3
+              w-2/5 max-md:w-4/5 max-lg:w-1/2 max-sm:mt-3
+              gap-10 max-lg:gap-5 max-md:gap-3 max-sm:gap-2">
             <h2
-              className="text-white text-5xl font-medium"
+              className="text-white text-5xl max-md:text-4xl max-sm:text-2xl font-medium"
             >Faites-vous plaisir tout en prenant soin de votre santé et de la planète.</h2>
 
-            <p className="text-white text-md">
-              Bienvenue chez Behati, la marque qui vous propose des produits agroalimentaires 100% bio, cultivés sans engrais dans les hautes montagnes de l’ouest Cameroun.
+            <p className="text-white text-md max-sm:text-sm">
+              Bienvenue chez Behati, la marque qui vous propose des produits agroalimentaires 100% bio,
+              cultivés sans engrais dans les hautes montagnes de l’ouest Cameroun.
             </p>
-            <div className="flex flex-1 space-x-3">
+            <div className="flex flex-1 space-x-3 max-sm:mt-4">
               <CustomButton
                 title='Contactez-nous'
-                containerStyle='primary_green h-9 rounded-full text-sm font-bold px-4'
+                containerStyle='primary_green h-9 max-sm:w-full rounded-full text-sm font-bold px-4'
                 handleClick={() => { }}
               />
               <CustomButton
                 title='Voir nos produits'
-                containerStyle='border-2 h-9 rounded-full text-white text-sm font-bold px-4'
+                containerStyle='border-2 h-9 rounded-full text-white text-sm font-bold px-4 max-sm:hidden'
                 handleClick={() => { }}
               />
             </div>
