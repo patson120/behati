b067d0fbdbd6416f31266d6764088b675bd4e7ce
patson120 <patrickkennenl@gmail.com>
2024-02-26 09:17:15 +0100

    Mise à jour du responsive au footer 1

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ const Home = () => {
       {/* Section B */}
       <section className="w-full mt-20 max-sm:-mt-12 mb-12">
         <div className="flex justify-center items-center">
-          <div className="w-1/3 md:w-3/5 max-md:w-2/3 text-center max-sm:text-left max-sm:w-4/5 space-y-2">
+          <div className="w-1/3 md:w-2/5 max-md:w-3/5 text-center max-sm:text-left max-sm:w-4/5">
             <p className="text-4xl max-sm:text-xl font-bold">Nos produits phares</p>
             <p className="text-md  max-sm:text-xs">Des produits sains, savoureux et pratiques, qui repondent à vos besoins nutritionnels et à vos envies gourmandes.</p>
           </div>
@@ -135,7 +135,7 @@ const Home = () => {
       {/* Section C */}
       <section className="container max-sm:mt-8 max-md:mt-48 mb-24 max-md:mb-0 md:mb-0">
         <div className="bg_image_map flex-center max-md:h-1/2">
-          <div className="flex-center w-2/5 md:w-3/4 max-md:w-3/4 max-sm:w-full ">
+          <div className="flex-center w-1/3 md:w-3/5 max-md:w-4/5 max-sm:w-full ">
             <div className="text-center max-sm:text-left space-y-5">
               <p className="text-4xl max-sm:text-xl max-sm:text-left font-bold">Nous vous livrons partout dans le monde</p>
               <p className="text-md max-sm:text-xs">
@@ -156,7 +156,7 @@ const Home = () => {
 
       {/* Section D */}
       <section className="container h-screen max-sm:my-24 max-sm:h-min md:h-svh flex-center">
-        <div className="flex-center w-2/5 md:w-4/5 max-md:w-4/5 max-sm:w-full">
+        <div className="flex-center w-1/3 md:w-3/5 max-md:w-4/5 max-sm:w-full">
           <div className="text-center max-sm:text-left space-y-5">
             <p className="text-4xl max-sm:text-xl font-bold">Nous sommes ouverts pour la distribution de nos produits</p>
             <p className="text-md max-sm:text-xs">
