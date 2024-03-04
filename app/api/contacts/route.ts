5ca792b36876e643480eec72db03fcd67959ace5
patson120 <patrickkennenl@gmail.com>
2024-03-04 13:22:54 +0100

    Suppression de la fonction d'envoi de mail

--- app/api/contacts/route.ts
+++ app/api/contacts/route.ts
@@ -18,22 +18,25 @@ export async function POST(request: Request) {
     const hookContact = useContact()
     const response = await hookContact.save(body)
 
-    const hookSenMail = useSendMail()
-    const status = await hookSenMail.send(
-        body.email,
-        body.object,
-        body.message,
-        `${body.nom} ${body.prenom}`,
-    )
+    // const hookSenMail = useSendMail()
+    // const status = await hookSenMail.send(
+    //     body.email,
+    //     body.object,
+    //     body.message,
+    //     `${body.nom} ${body.prenom}`,
+    // )
     
-    if (status === true) {
+    // if (status === true) {
 
-        console.log("Mail sent successfully");
+    //     console.log("Mail sent successfully");
         
-        return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
-    }
-    console.log("Unable to send mail");
+    //     return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
+    // }
+    // console.log("Unable to send mail");
     
-    return NextResponse.json({ success: false, status: STATUS.BAD_REQUEST, result: response }, { status: STATUS.BAD_REQUEST })
+    // return NextResponse.json({ success: false, status: STATUS.BAD_REQUEST, result: response }, { status: STATUS.BAD_REQUEST })
+
+
+    return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
 
 }
