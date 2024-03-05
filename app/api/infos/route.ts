dd38920bf586972a954215af4d2639a9b1f14d72
patson120 <patrickkennenl@gmail.com>
2024-03-05 08:28:46 +0100

    Mise à jour de l'api de infos

--- app/api/infos/route.ts
+++ app/api/infos/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 
 import STATUS from "@/utils/status.code";
 import useContact from "@/hooks/useContact";
+import useSendMail from "@/hooks/useSendMail";
 
 
 // Handles POST requests to /api/infos
@@ -12,7 +13,21 @@ export async function POST(request: Request) {
     const hookContact = useContact()
     const response = await hookContact.save(body)
 
+    
+    const hookSenMail = useSendMail()
+    const status = await hookSenMail.send(
+        body.email,
+        body.object,
+        body.message,
+        `${body.nom} ${body.prenom}`,
+    )
+    
+    if (status === true) {
+        console.log("Mail sent successfully");
+        return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
+    }
+    console.log("Unable to send mail");
+    return NextResponse.json({ success: false, status: STATUS.BAD_REQUEST, result: response }, { status: STATUS.BAD_REQUEST })
 
-    return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
-
+    // return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
 }
