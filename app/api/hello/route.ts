3cd7481623e158b6f3c7f6262216085a924a8d73
patson120 <patrickkennenl@gmail.com>
2024-03-05 10:23:59 +0100

    Ajout du endpoint hello

---
+++ app/api/hello/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server"
+
+import STATUS from '@/utils/status.code'
+
+
+export async function POST(request: Request) {
+
+    const response = [
+        {
+            id: 1,
+            name: "John"
+        },
+        {
+            id: 2,
+            name: "Smith"
+        }
+    ]
+
+    return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
+
+}
