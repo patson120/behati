bfe73bd8ee3e1748484ffdebde0af73516ea3ffe
patson120 <patrickkennenl@gmail.com>
2024-03-06 20:14:54 +0100

    Mise à jour complète du projet

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,5 +1,4 @@
 
-
 const BASE_URL = process.env.NODE_ENV == "production" ? process.env.NEXT_PUBLIC_BASE_URL : "http://localhost:3000/api"
 const EMAIL_USERNAME = process.env.NEXT_PUBLIC_EMAIL
 const EMAIL_PASSWORD = process.env.NEXT_PUBLIC_EMAIL_PASS
