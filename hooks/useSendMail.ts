645c8b7d6847ff3a668907809030dd2477db49c9
patson120 <patrickkennenl@gmail.com>
2024-03-06 18:14:44 +0100

    Ajout de la possibilité d'envoi des accusées de réception des mails de pré-commande en ligne

--- hooks/useSendMail.ts
+++ hooks/useSendMail.ts
@@ -10,7 +10,7 @@ const useSendMail = () => {
         try {
             await transporter.sendMail({
                 from: from,
-                to: 'claudkuate@gmail.com', // CONSTANTS.EMAIL_USERNAME,,
+                to: 'claudkuate@gmail.com', // CONSTANTS.EMAIL_USERNAME,
                 subject: subject,
                 text: message,
                 html: `
@@ -25,11 +25,40 @@ const useSendMail = () => {
                     </html>
                 `
             })
-            return true
+            return await sendReponse(`${CONSTANTS.EMAIL_USERNAME}`, from, "Accusé de réception", message, username)
         } catch (error) {
             return false
         }
     }
+
+
+    const sendReponse = async (from: string, to: string, subject: string, message: string, username: string) => {
+        try {
+            await transporter.sendMail({
+                from: from,
+                to: to,
+                subject: subject,
+                text: message,
+                html: `
+                    <html lang='en'>
+                        <body>
+                            <main>
+                                <h1 style="font-weight: bold">Behati</h1> <br/>
+                                <p style="font-weight: bold">Mr./Mme ${username}, </p>
+                                <p>Nous accusons réception de votre message depuis la plareforme <b>Behati</b> au sujet << ${message} >></p>
+                                <p>Nous nous vous reviendrons bientôt pour plus d'informations. <br/> Merci de votre confiance vis-à-vis de nos produit</p>
+                            </main>
+                        </body>
+                    </html>
+                `
+            })
+            return true
+        } catch (error) {
+            console.log(2, error);
+            return false
+        }
+    }
+
 
     return { send }
 }
