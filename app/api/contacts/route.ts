4ac935758f54f8df9eb56fccf62885c5ead4a995
patson120 <patrickkennenl@gmail.com>
2024-03-07 08:29:02 +0100

    Mise à jour de la méthode post de l'api des contacts

--- app/api/contacts/route.ts
+++ app/api/contacts/route.ts
@@ -1,6 +1,5 @@
 import useContact from "@/hooks/useContact";
 import { NextResponse } from "next/server";
-
 import STATUS from "@/utils/status.code"
 import useSendMail from "@/hooks/useSendMail";
 
@@ -10,7 +9,6 @@ export async function POST(request: Request) {
     const body = await request.json()
     const hookContact = useContact()
     const response = await hookContact.save(body)
-
     const hookSenMail = useSendMail()
     const status = await hookSenMail.send(
         body.email,
@@ -18,10 +16,8 @@ export async function POST(request: Request) {
         body.message,
         `${body.nom} ${body.prenom}`,
     )
-    
     if (status === true) {
         return NextResponse.json({ success: true, status: 201, result: response }, { status: STATUS.CREATED })
     }
-    
     return NextResponse.json({ success: false, status: STATUS.BAD_REQUEST, result: response }, { status: STATUS.BAD_REQUEST })
 }
